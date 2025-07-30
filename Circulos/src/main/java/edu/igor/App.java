20ef045c957ad6fa23c666959a64318889945830
Igor Xarless <95253660+Xarrles@users.noreply.github.com>
2025-07-30 13:56:40 +0000

    feat: Estrutura projeto

---
+++ Circulos/src/main/java/edu/igor/App.java
@@ -0,0 +1,7 @@
+package edu.igor;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto Circulos criado com sucesso.");
+    }
+}
---
+++ Circulos/src/main/java/edu/igor/Circulo.java
@@ -0,0 +1,7 @@
+package edu.igor;
+
+public class Circulo{
+    public static void Main(String args[]){
+        System.out.print("Hello world~~ este");
+    }
+}
