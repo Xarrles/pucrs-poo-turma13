edbe4ebf5dc64b16253e3abd0ddc65129010ccbc
Igor Xarless <95253660+Xarrles@users.noreply.github.com>
2025-08-04 13:52:00 +0000

    feat: Estruturado classe Circulo e testado em App

--- Circulos/src/main/java/edu/igor/Circulo.java
+++ Circulos/src/main/java/edu/igor/Circulo.java
@@ -1,7 +1,40 @@
 package edu.igor;
 
 public class Circulo{
-    public static void Main(String args[]){
-        System.out.print("Hello world~~ este");
+    private double coordX;
+    private double coordY;
+    private double raio;
+
+    public Circulo(){
+        this.coordX = 0;
+        this.coordY = 0;
+        raio   = 1;
+    }
+
+    public Circulo(double umX, double umY, double umRaio){
+        this.coordX = umX;
+        this.coordY = umY;
+        this.raio   = umRaio;
+    }
+
+    @Override
+    public String toString(){
+        return "X: " + coordX + " Y: " + coordY + " Raio: " + raio;
+    }
+
+    public void Zoom(double fator) {
+        this.raio *= fator;
+    }
+
+    public void Mover(double x, double y){
+        this.coordX = x;
+        this.coordY = y;
+    }
+
+    public double Area(){
+        double area = 3.14 * this.raio * this.raio;
+        //System.out.print("Area do ciruclo é: " +  area);
+
+        return area;
     }
 }
