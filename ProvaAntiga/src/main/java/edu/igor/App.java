061b9ced8ed41c9ef842d9f6a0a3e5605dde5f86
Igor Xarless <95253660+Xarrles@users.noreply.github.com>
2025-09-22 13:20:15 +0000

    revisaoprova

---
+++ ProvaAntiga/src/main/java/edu/igor/App.java
@@ -0,0 +1,7 @@
+package edu.igor;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto ProvaAntiga criado com sucesso.");
+    }
+}
---
+++ ProvaAntiga/src/main/java/edu/igor/CadastroImoveis.java
@@ -0,0 +1,7 @@
+package edu.igor;
+
+public class CadastroImoveis {
+    
+public String private list(){
+    return "Listas de imoveis";
+}
